d3f372726ec6dedc42e54bd3e48719afeb5b6fdb
Abdulaziz930 <abdulazizak@code.edu.az>
2021-10-06 21:36:52 +0400

    set initial project settings

---
+++ DataAccess/Concret/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concret
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
