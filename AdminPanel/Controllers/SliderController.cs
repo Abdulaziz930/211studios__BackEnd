eddfc0117f8e13b628046778c362a7ee5e264548
Abdulaziz930 <abdulazizak@code.edu.az>
2021-10-10 15:37:59 +0400

    slider's update page is done

--- AdminPanel/Controllers/SliderController.cs
+++ AdminPanel/Controllers/SliderController.cs
@@ -4,6 +4,7 @@ using Entities.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Utils;
@@ -86,6 +87,75 @@ namespace AdminPanel.Controllers
             }
 
             await _sliderService.AddAsync(slider);
+
+            return RedirectToAction("Index");
+        }
+
+        #endregion
+
+        #region Update
+
+        public async Task<IActionResult> Update(int? id)
+        {
+            if (id is null)
+                return BadRequest();
+
+            var slider = await _sliderService.GetSliderAsync(id.Value);
+            if (slider is null)
+                return NotFound();
+
+            return View(slider);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(int? id, Slider slider)
+        {
+            if (id is null)
+                return BadRequest();
+
+            if (id != slider.Id)
+                return BadRequest();
+
+            var dbSlider = await _sliderService.GetSliderAsync(id.Value);
+            if (dbSlider is null)
+                return NotFound();
+
+            var fileName = dbSlider.Image;
+
+            if (slider.Photo != null)
+            {
+                if (!slider.Photo.IsImage())
+                {
+                    ModelState.AddModelError("Photo", "This is not a picture");
+                    return View();
+                }
+
+                if (!slider.Photo.IsSizeAllowed(3000))
+                {
+                    ModelState.AddModelError("Photo", "The size of the image you uploaded is 3 MB higher.");
+                    return View();
+                }
+
+                var path = Path.Combine(Constants.ImageFolderPath, dbSlider.Image);
+
+                if (System.IO.File.Exists(path))
+                {
+                    System.IO.File.Delete(path);
+                }
+
+                fileName = await FileUtil.GenerateFileAsync(Constants.ImageFolderPath, slider.Photo);
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(dbSlider);
+            }
+
+            dbSlider.Title = slider.Title;
+            dbSlider.Image = fileName;
+
+            await _sliderService.UpdateAsync(dbSlider);
 
             return RedirectToAction("Index");
         }
