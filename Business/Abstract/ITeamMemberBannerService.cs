b1f10d5ffae99e334929f8290abd1c4f36b4978e
Abdulaziz930 <abdulazizak@code.edu.az>
2021-10-25 18:33:21 +0400

    team member detail banner's CRUD is done

---
+++ Business/Abstract/ITeamMemberBannerService.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface ITeamMemberBannerService
+    {
+        Task<TeamMemberBanner> GetTeamMemberBannerAsync(int id);
+
+        Task<List<TeamMemberBanner>> GetTeamMembersAsync();
+
+        Task<bool> AddAsync(TeamMemberBanner teamMemberBanner);
+
+        Task<bool> UpdateAsync(TeamMemberBanner teamMemberBanner);
+
+        Task<bool> DeleteAsync(int id);
+    }
+}
